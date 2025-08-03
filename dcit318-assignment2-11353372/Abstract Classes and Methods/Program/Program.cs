27282d02c986c499004b64171708db8f64a5830f
mighty808 <effahaboagye888@gmail.com>
2025-08-03 23:52:12 +0000

    started and finished Assignment 2

---
+++ dcit318-assignment2-11353372/Abstract Classes and Methods/Program/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Abstract Working...");
+
+        Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle();
+        // Shape shape = new Shape();
+
+        circle.GetArea();
+        rectangle.GetArea();
+        // shape.GetArea();
+    }
+}
+
+abstract class Shape
+{
+    public void GetArea()
+    {
+        Console.WriteLine("Checking Area");
+    }
+}
+
+
+
+class Circle : Shape
+{
+    public void GetArea()
+    {
+        Console.WriteLine("Abstract Circle");
+    }
+}
+
+
+
+class Rectangle : Shape
+{
+    public void GetArea()
+    {
+        Console.WriteLine("Abstract Rectangle");
+    }
+}
---
+++ dcit318-assignment2-11353372/Inheritance and method overloading/Program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program 
+{
+    static void Main() {
+        Console.WriteLine("Working....");
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
+
+class Animal {
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("The Animal goes *rarrr*");
+    }
+}
+
+
+class Dog : Animal {
+    public override void MakeSound ()
+    {
+        Console.WriteLine("The dog goes *woof*");
+        Console.WriteLine("The dog Barks");
+    }
+}
+
+class Cat : Animal {
+    public override void MakeSound ()
+    {
+        Console.WriteLine("The Cat goes *meow*");
+        Console.WriteLine("The Cat *meows*");
+    }
+}
