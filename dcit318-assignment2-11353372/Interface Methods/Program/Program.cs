e2193338895e5618945166a55c71cdfe1fbf9ddc
mighty808 <effahaboagye888@gmail.com>
2025-08-03 23:51:45 +0000

    started and finished Assignment 2

---
+++ dcit318-assignment2-11353372/Interface Methods/Program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Interfaces Running...");
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+
+    }
+}
+
+    interface IMovable
+    {
+        void Move();
+    }
+
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is Moving...");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
